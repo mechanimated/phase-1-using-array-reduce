b1f47bbb10541c9a9fd036eac0aa8f6b385991b7
Liz Boika <mdaicdxl@gmail.com>
2022-07-25 14:29:36 -0400

    commit tree b7d331d75d96b95c8318befa8e5b2a7b18cefac9

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+let totalBatteries = batteryBatches.reduce((x, y) => x + y, 0)
+console.log(totalBatteries(batteryBatches));
 
-// Code your solution here
+
